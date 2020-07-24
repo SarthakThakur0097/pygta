f5f14b28e3b302eaa34b72c69df57594d727d320
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-07-23 20:55:34 -0400

    Getting gta 5 windowe

---
+++ pygta.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+last_time = time.time()
+while(True):
+    printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
+    printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
+    
+    print('Loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('window', np.array(printscreen_pil))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
